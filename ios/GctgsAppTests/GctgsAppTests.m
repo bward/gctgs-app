d6be1676e83f0668696442c3077f9488bd0a0b3c
Ben Ward <bw@ben-ward.net>
2016-09-28 21:09:57 +0100

    Upgraded react-native

--- ios/GctgsAppTests/GctgsAppTests.m
+++ ios/GctgsAppTests/GctgsAppTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface GctgsAppTests : XCTestCase
+@interface gctgsappTests : XCTestCase
 
 @end
 
-@implementation GctgsAppTests
+@implementation gctgsappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
